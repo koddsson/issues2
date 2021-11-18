f63e40425a1d6ba8cda538941aa79652c5790af5
Kristján Oddsson <koddsson@gmail.com>
2021-11-18 21:53:01 +0000

    Include Primer ViewComponent JavaScript in the app

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,11 @@ pin "@hotwired/turbo-rails", to: "turbo.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@primer/view-components", to: "https://ga.jspm.io/npm:@primer/view-components@0.0.61/app/components/primer/primer.js"
+pin "@github/auto-complete-element", to: "https://ga.jspm.io/npm:@github/auto-complete-element@3.0.2/dist/index.js"
+pin "@github/clipboard-copy-element", to: "https://ga.jspm.io/npm:@github/clipboard-copy-element@1.1.2/dist/index.umd.js"
+pin "@github/combobox-nav", to: "https://ga.jspm.io/npm:@github/combobox-nav@2.0.2/dist/index.js"
+pin "@github/details-menu-element", to: "https://ga.jspm.io/npm:@github/details-menu-element@1.0.12/dist/index.js"
+pin "@github/image-crop-element", to: "https://ga.jspm.io/npm:@github/image-crop-element@5.0.0/dist/index.js"
+pin "@github/tab-container-element", to: "https://ga.jspm.io/npm:@github/tab-container-element@3.1.2/dist/index.js"
+pin "@github/time-elements", to: "https://ga.jspm.io/npm:@github/time-elements@3.1.2/dist/index.js"
