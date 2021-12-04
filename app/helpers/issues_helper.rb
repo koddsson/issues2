3d5d413da8ae9b244f083a4d173bcd1b35a5536d
Kristján Oddsson <koddsson@gmail.com>
2021-12-04 22:14:55 +0000

    Implement rudimentary read-only reactions

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,2 +1,21 @@
 module IssuesHelper
+  def key_to_emoji(key)
+    if key == :"+1"
+      "👍"
+    elsif key == :"-1"
+      "👎"
+    elsif key == :"laugh"
+      "😂"
+    elsif key == :"hooray"
+      "🎉"
+    elsif key == :"confused"
+      "😕"
+    elsif key == :"heart"
+      "❤️"
+    elsif key == :"rocket"
+      "🚀"
+    elsif key == :"eyes"
+      "👀"
+    end
+  end
 end
